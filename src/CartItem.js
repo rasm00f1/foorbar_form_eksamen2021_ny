e5d50e3476bc4a3329aaa0640430a788162ab934
rasmus petersen <rasm00f1@stud.kea.dk>
2021-05-24 10:44:20 +0200

    cart fully funcitonal i think

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,12 +1,13 @@
 import OverviewButton from "./OverviewButton";
 export default function CartItem(props) {
   let beerimage = "";
-  //ForEach beertype in the list check if beer == beerType.name
+  //ForEach beertype in the list check if beer == beerType.name, find the corresponding img
   props.beerTypesList.forEach((beerType) => {
     if (props.beer === beerType.name) {
       beerimage = beerType.label;
     }
   });
+  //ForEach beer on tap check if beer == price.beername, find the corresponding price and set it for each item
   let beerPrice = 0;
   props.prices.map((price) => {
     if (props.beer === price.beername) {
@@ -14,12 +15,26 @@ export default function CartItem(props) {
     }
     return beerPrice;
   });
+  //Calculate the total price based on amount of beers in the cart
   let totalBeerPrice = beerPrice * props.amount;
+
+  //Removes all beers of one time from cart when close button clicked
+  function removedThisArray() {
+    const newArray = props.cartItems.filter((item) => {
+      if (item.beer === props.beer) {
+        return false;
+      }
+      return true;
+    });
+    props.setCartItems(newArray);
+  }
 
   return (
     <div>
       <div className="beer_container">
-        <button className="close">Close</button>
+        <button onClick={removedThisArray} className="close">
+          Close
+        </button>
 
         <div className="image_container">
           <img src={"./img/" + beerimage} alt="" />
